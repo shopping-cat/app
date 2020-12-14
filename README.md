b6b66326f503de511b3938a888f055806db770ae
hyun <coderhyun476@gmail.com>
2020-12-14 14:21:14 +0900

    ios font

--- README.md
+++ README.md
@@ -6,6 +6,4 @@
 - android/app/shoppingcat-user-app.keystore
 - android/app/google-services.json // 파이어베이스에서 다운받아주세요
 - ios GoogleServices-info.plist // 파이어베이스에서 다운받아주세요
-
-## TODO
-- font
+    
--- src/components/BaseText.tsx
+++ src/components/BaseText.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, TextProps, View } from 'react-native'
 
-const BaseText: React.FC<TextProps> = (props) => <Text style={[styles.baseStyle, props.style]} {...props} />
+const BaseText: React.FC<TextProps> = (props) => <Text  {...props} style={[styles.baseStyle, props.style]} />
 
 export default BaseText
 
 const styles = StyleSheet.create({
     baseStyle: {
-
+        fontFamily: 'BMJUA'
     }
 })
 export const baseTextStyle = styles.baseStyle
