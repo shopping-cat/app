1f5f8ad1131cf9a4f00387040c804a7e33d0da03
hyun <coderhyun476@gmail.com>
2021-05-13 18:13:54 +0900

    change company info

--- src/components/Layouts/CompanyInfo.tsx
+++ src/components/Layouts/CompanyInfo.tsx
@@ -3,10 +3,9 @@ import { StyleSheet, Text, View } from 'react-native'
 import { GRAY, VERY_LIGHT_GRAY, WIDTH } from '../../constants/styles'
 import BaseText from '../Text/BaseText'
 
-const INFO = `대표자 남궁현 | 사업자등록번호 000-0000-000
-통신판매중개업 서울 송파-1243
-경기도 용인시 기흥로 116번길 77
-통신판매업 
+const INFO = `대표자 남궁현 | 사업자등록번호 698-47-00632
+경기도 용인시 기흥로 116번길 77 1401호
+통신판매업
 
 쇼핑냥이는 통신판매중개자이며 통신판매의 당사자가 아닙니다. 따라서 쇼핑냥이는 상품/거래 정보 및 거래에 책임을 지지 않습니다`
 
