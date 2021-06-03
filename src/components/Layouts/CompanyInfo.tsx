a10cef221b82a7eda2b8ef2589f0096f10487e08
hyun <coderhyun476@gmail.com>
2021-06-03 23:25:54 +0900

    update company info

--- src/components/Layouts/CompanyInfo.tsx
+++ src/components/Layouts/CompanyInfo.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { GRAY, VERY_LIGHT_GRAY, WIDTH } from '../../constants/styles'
+import { IS_CAT } from '../../constants/values'
 import BaseText from '../Text/BaseText'
 
-const INFO = `대표자 남궁현 | 사업자등록번호 698-47-00632
-대표자번호 050-7465-0492
-사업장주소 경기도 용인시 기흥로 116번길 77 607동 1401호
+const INFO = `대표자 홍성욱 | 사업자등록번호 131411-0441255
+대표자번호 010-6613-0703
+사업장주소 서울특별시 강남구 테헤란로44길 8, 6층 57-2호
 통신판매업
 
-쇼핑냥이는 통신판매중개자이며 통신판매의 당사자가 아닙니다. 따라서 쇼핑냥이는 상품/거래 정보 및 거래에 책임을 지지 않습니다`
+${IS_CAT ? '쇼핑냥이' : '쇼핑댕이'}는 통신판매중개자이며 통신판매의 당사자가 아닙니다. ${IS_CAT ? '쇼핑냥이' : '쇼핑댕이'}는 상품/거래 정보 및 거래에 책임을 지지 않습니다`
 
 const CompanyInfo = () => {
     return (
         <View style={styles.container} >
-            <BaseText style={styles.name} >쇼핑냥이</BaseText>
+            <BaseText style={styles.name} >딥스펀지 주식회사</BaseText>
             <BaseText selectable style={styles.info} >{INFO}</BaseText>
         </View>
     )
